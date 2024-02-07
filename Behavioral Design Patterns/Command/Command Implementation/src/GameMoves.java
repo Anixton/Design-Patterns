398427b1fa13016999faf8f06100f340d8a59fc7
Anixton <hakangezginci35@gmail.com>
2024-02-07 20:55:42 +0300

    Refactor comments in GameMoves class

--- Behavioral Design Patterns/Command/Command Implementation/src/GameMoves.java
+++ Behavioral Design Patterns/Command/Command Implementation/src/GameMoves.java
@@ -1,17 +1,17 @@
 /*
- * This program demonstrates the Command pattern in a game context, providing functionalities such as moving a unit, 
- * undoing and redoing moves, and replaying the game.
+ * This program demonstrates the Command pattern in a game context, providing functionalities 
+ * such as moving a unit, undoing and redoing moves, and replaying the game.
  *
  * Key components:
  * - Game: The client class that creates and executes commands. It also manages undo/redo operations and provides 
  *         methods to display the current position and replay the game.
- * - Unit: The receiver class that performs actions such as moving to a new position, moving back to the previous position, 
- *         moving forward to the next position, and replaying the game.
- * - MoveUnitCommand: A concrete command implementing the Command interface. It provides methods to execute, undo, 
- *         and redo the command, and to get the destination and unit.
+ * - Unit: The receiver class that performs actions such as moving to a new position, moving back to the 
+ *         previous position, moving forward to the next position, and replaying the game.
+ * - MoveUnitCommand: A concrete command implementing the Command interface. It provides methods to 
+ *         execute, undo, and redo the command, and to get the destination and unit.
  * - Position: A utility class representing a position with x and y coordinates.
- * - GameMoves: The main class that orchestrates the game flow, including moving the unit, undoing and redoing moves, 
- *         displaying the current position, and replaying the game.
+ * - GameMoves: The main class that orchestrates the game flow, including moving the unit, 
+ *         undoing and redoing moves, displaying the current position, and replaying the game.
  */
 
 public class GameMoves {
